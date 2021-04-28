7aef41f193fe541578da92091cc676554ea1d251
MrJuanGaviriaK <juan.gaviria@kommit.co>
2021-04-28 08:03:21 -0500

    Delete unused code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-console.log("Hola Juan")
-
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
